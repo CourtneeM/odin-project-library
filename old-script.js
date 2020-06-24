d4785c2450ea58a68504c87507f5a393f84f6832
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-06-23 19:31:18 -0500

    Refactor Render to combine methods into one useable method

--- old-script.js
+++ old-script.js
@@ -4,19 +4,16 @@ const newBookBtn = document.getElementById("new-book-btn");
 newBookForm.style.display = "none";
 let myLibrary = [];
 
-class Book {
-  constructor(author, title, pages, read, id) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.id = id;
-  }
+function Book(author, title, pages, read, id) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+  this.id = id;
 }
 
-class BookActions {
-  
-  addBookToLibrary(e) {
+let bookActions = {
+  addBookToLibrary: function(e) {
     let newBookAuthor = document.getElementById("new-book-author");
     let newBookTitle = document.getElementById("new-book-title");
     let newBookPages = document.getElementById("new-book-pages");
@@ -31,11 +28,9 @@ class BookActions {
       newBookForm.style.display = "none";
     }
     myLibrary.push(newBook);
-    render.resetLibrary();
-    render.displayBookInLibrary();
-  };
-
-  updateBookId() {
+    render();
+  },
+  updateBookId: function() {
     for(let book in myLibrary) {
       for(let prop in myLibrary[book]) {
         if(prop === "id") {
@@ -43,69 +38,57 @@ class BookActions {
         }
       }
     }
-  };
-  
-  removeBook(e) {
+  },
+  removeBook: function(e) {
     myLibrary.splice(e.target.parentElement.id, 1);
     this.updateBookId();
-    render.resetLibrary();
-    render.displayBookInLibrary();
-  };
-
-  toggleReadStatus(e) {
+    render();
+  },
+  toggleReadStatus: function(e) {
     let currentBookIndex = +(e.target.parentElement.id);
     if(myLibrary[currentBookIndex].read === true) {
       myLibrary[currentBookIndex].read = false;
     } else if(myLibrary[currentBookIndex].read === false) {
       myLibrary[currentBookIndex].read = true;
     }
-    render.resetLibrary();
-    render.displayBookInLibrary();
-  };
-
+    render();
+  }
 }
 
-class Render {
-
-  resetLibrary() {
-    if(document.querySelector("#book-container").childNodes.length > 0) {
-      bookContainer.querySelectorAll("*").forEach(n => n.remove());
-    }
-  };
-
-  displayBookInLibrary() {
-    for(let book in myLibrary) {
-      let div = document.createElement("div");
-      let currentBook = div;
-      for(let prop in myLibrary[book]) {
-        let p = document.createElement('p');
-        if(prop !== "id" && prop !== "read") { 
-          p.textContent = `${myLibrary[book][prop]}`;
+function render() {
+  if(document.querySelector("#book-container").childNodes.length > 0) {
+    bookContainer.querySelectorAll("*").forEach(n => n.remove());
+  }
+  for(let book in myLibrary) {
+    let div = document.createElement("div");
+    let currentBook = div;
+    for(let prop in myLibrary[book]) {
+      let p = document.createElement('p');
+      if(prop !== "id" && prop !== "read") { 
+        p.textContent = `${myLibrary[book][prop]}`;
+        currentBook.appendChild(p);
+      }
+      if(prop === "read") {
+        if(myLibrary[book][prop] === true) {
+          p.textContent = "Read";
+          currentBook.appendChild(p);
+        } else if(myLibrary[book][prop] === false) {
+          p.textContent = "Not Read";
           currentBook.appendChild(p);
         }
-        if(prop === "read") {
-          if(myLibrary[book][prop] === true) {
-            p.textContent = "Read";
-            currentBook.appendChild(p);
-          } else if(myLibrary[book][prop] === false) {
-            p.textContent = "Not Read";
-            currentBook.appendChild(p);
-          }
-        }
-        if(prop === "id") {
-          currentBook.id = myLibrary[book][prop];
-        }
       }
-        let p = document.createElement("p");
-        p.textContent = "Remove Book";
-        p.id = "remove-book-btn";
-        p.style.backgroundColor = "red";
-        p.style.color = "white";
-        currentBook.appendChild(p);
-        bookContainer.appendChild(currentBook);
+      if(prop === "id") {
+        currentBook.id = myLibrary[book][prop];
+      }
     }
-  };
-
+      let p = document.createElement("p");
+      p.textContent = "Remove Book";
+      p.id = "remove-book-btn";
+      p.style.backgroundColor = "red";
+      p.style.color = "white";
+      currentBook.appendChild(p);
+      bookContainer.appendChild(currentBook);
+  }
 }
 
 
@@ -128,6 +111,3 @@ document.addEventListener('click', e => {
     bookActions.toggleReadStatus(e);
   }
 })
-
-let render = new Render();
-let bookActions = new BookActions();
--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const newBookBtn = document.getElementById("new-book-btn");
 newBookForm.style.display = "none";
 let myLibrary = [];
 
-function Book(author, title, pages, read, id) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-  this.id = id;
+class Book {
+  constructor(author, title, pages, read, id) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+  }
 }
 
-let bookActions = {
-  addBookToLibrary: function(e) {
+class BookActions {
+  
+  addBookToLibrary(e) {
     let newBookAuthor = document.getElementById("new-book-author");
     let newBookTitle = document.getElementById("new-book-title");
     let newBookPages = document.getElementById("new-book-pages");
@@ -28,9 +31,10 @@ let bookActions = {
       newBookForm.style.display = "none";
     }
     myLibrary.push(newBook);
-    render();
-  },
-  updateBookId: function() {
+    render.renderLibrary();
+  };
+
+  updateBookId() {
     for(let book in myLibrary) {
       for(let prop in myLibrary[book]) {
         if(prop === "id") {
@@ -38,57 +42,72 @@ let bookActions = {
         }
       }
     }
-  },
-  removeBook: function(e) {
+  };
+  
+  removeBook(e) {
     myLibrary.splice(e.target.parentElement.id, 1);
     this.updateBookId();
-    render();
-  },
-  toggleReadStatus: function(e) {
+    render.renderLibrary();
+  };
+
+  toggleReadStatus(e) {
     let currentBookIndex = +(e.target.parentElement.id);
     if(myLibrary[currentBookIndex].read === true) {
       myLibrary[currentBookIndex].read = false;
     } else if(myLibrary[currentBookIndex].read === false) {
       myLibrary[currentBookIndex].read = true;
     }
-    render();
-  }
+    render.renderLibrary();
+  };
+
 }
 
-function render() {
-  if(document.querySelector("#book-container").childNodes.length > 0) {
-    bookContainer.querySelectorAll("*").forEach(n => n.remove());
-  }
-  for(let book in myLibrary) {
-    let div = document.createElement("div");
-    let currentBook = div;
-    for(let prop in myLibrary[book]) {
-      let p = document.createElement('p');
-      if(prop !== "id" && prop !== "read") { 
-        p.textContent = `${myLibrary[book][prop]}`;
-        currentBook.appendChild(p);
-      }
-      if(prop === "read") {
-        if(myLibrary[book][prop] === true) {
-          p.textContent = "Read";
-          currentBook.appendChild(p);
-        } else if(myLibrary[book][prop] === false) {
-          p.textContent = "Not Read";
+class Render {
+
+  resetLibrary() {
+    if(document.querySelector("#book-container").childNodes.length > 0) {
+      bookContainer.querySelectorAll("*").forEach(n => n.remove());
+    }
+  };
+
+  displayBookInLibrary() {
+    for(let book in myLibrary) {
+      let div = document.createElement("div");
+      let currentBook = div;
+      for(let prop in myLibrary[book]) {
+        let p = document.createElement('p');
+        if(prop !== "id" && prop !== "read") { 
+          p.textContent = `${myLibrary[book][prop]}`;
           currentBook.appendChild(p);
         }
+        if(prop === "read") {
+          if(myLibrary[book][prop] === true) {
+            p.textContent = "Read";
+            currentBook.appendChild(p);
+          } else if(myLibrary[book][prop] === false) {
+            p.textContent = "Not Read";
+            currentBook.appendChild(p);
+          }
+        }
+        if(prop === "id") {
+          currentBook.id = myLibrary[book][prop];
+        }
       }
-      if(prop === "id") {
-        currentBook.id = myLibrary[book][prop];
-      }
+        let p = document.createElement("p");
+        p.textContent = "Remove Book";
+        p.id = "remove-book-btn";
+        p.style.backgroundColor = "red";
+        p.style.color = "white";
+        currentBook.appendChild(p);
+        bookContainer.appendChild(currentBook);
     }
-      let p = document.createElement("p");
-      p.textContent = "Remove Book";
-      p.id = "remove-book-btn";
-      p.style.backgroundColor = "red";
-      p.style.color = "white";
-      currentBook.appendChild(p);
-      bookContainer.appendChild(currentBook);
-  }
+  };
+
+  renderLibrary() {
+    this.resetLibrary();
+    this.displayBookInLibrary();
+  };
+
 }
 
 
@@ -96,11 +115,6 @@ document.addEventListener('click', e => {
   if(e.target.id === "add-book-btn") {
     bookActions.addBookToLibrary(e);
     newBookBtn.style.display = "block";
-  }
-
-  if(e.target.id === "new-book-btn") {
-    newBookForm.style.display = "block";
-    newBookBtn.style.display = "none";
   }
   
   if(e.target.id === "remove-book-btn") {
@@ -110,4 +124,13 @@ document.addEventListener('click', e => {
   if(e.target.textContent === "Read" || e.target.textContent === "Not Read") {
     bookActions.toggleReadStatus(e);
   }
+
+  if(e.target.id === "new-book-btn") {
+    newBookForm.style.display = "block";
+    newBookBtn.style.display = "none";
+  }
+
 })
+
+let render = new Render();
+let bookActions = new BookActions();
